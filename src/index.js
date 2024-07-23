ea31297a96716b435a23e7561a09a9339b45f5ac
Rhys Few <rhysfeew@gmail.com>
2024-07-23 11:03:53 +0000

    Installed axios, react-router-dom and bootstrap. Remove default react browser content.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
+    <Router>
     <App />
-  </React.StrictMode>
+    </Router >
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
